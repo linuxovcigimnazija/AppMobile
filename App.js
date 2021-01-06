ce617d655dc036475087119ff5c4309a608c3e1a
Nikola Vujkovic <NikolaVujke>
2021-01-06 18:36:25 +0100

    Setup for workflow

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <View style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
-        <MyProfileScreen />
+        <TravelInfoScreen />
       </SafeAreaView>
     </View>
   );
