eec32dac295f8dd69d4496b5e28bbcba7524810b
Sava Malinovic <savamalinovic@gmail.com>
2021-01-08 16:44:47 +0100

    Ubuntu font added, AppText component is usable

--- src/components/AppText.js
+++ src/components/AppText.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import {Text} from 'react-native';
+import Constants from '../constants/Constants';
 
 const AppText = (props) => {
-  return (
-    <Text
-      style={
-        ([
-          {
-            fontFamily: props.bold ? '' : '',
-            fontSize: props.size,
-            color: props.color,
-          },
-        ],
-        props.style)
-      }>
-      {props.children}
-    </Text>
-  );
+  const textStyle = {
+    fontFamily: props.bold ? 'Ubuntu-Bold' : 'Ubuntu-Regular',
+    fontSize: props.size ? props.size : 16,
+    color: props.color ? props.color : Constants.black,
+  };
+
+  return <Text style={[textStyle, props.style]}>{props.children}</Text>;
 };
 
 export default AppText;
