abf96e7eef485e1a50942b59f3d5bda9110b57fc
Sava Malinovic <savamalinovic@gmail.com>
2021-01-08 16:40:41 +0100

    AppText, Constants imported, first block created

--- src/screens/MyProfileScreen.js
+++ src/screens/MyProfileScreen.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import AppText from '../components/AppText'
+import Constants from '../constants/Constants';
 
 const MyProfileScreen = () => {
   return (
-    <View>
-      <Text>MyProfileScreen</Text>
+    <View style={styles.wholescreen}>
+      <View style={styles.upperContainer}>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  wholescreen:{
+    width: '100%',
+    height: '92%'
+  },
+  upperContainer: {
+    width: '100%',
+    height: '32%',
+    backgroundColor: Constants.primaryDark,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30
+  }
+});
 
 export default MyProfileScreen;
